935d126011e9ccf16548dd599e47dc8763b1af82
bro <percy.nguyen7@gmail.com>
2023-01-30 02:34:16 -0500

    update exercise

--- exercises/js/ex1.js
+++ exercises/js/ex1.js
@@ -2,6 +2,7 @@ const ball = document.querySelector('#ball');
 const smallCircle = document.querySelector('.result-area')
 const resultText = document.querySelector('#result-text');
 
+const rollSFX = new Audio('./assets/sounds/ball-roll.mp3');
 answers = [
     {yes:['It is certain','It is decidedly so','Without a doubt','Yes definitely','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes'],
     neutral:['Reply hazy, try again', 'Ask again later', 'Better not tell you now','Cannot predict now','Concentrate and ask again'],
@@ -24,14 +25,17 @@ const getRandomInt =()=> {
 let number;
 let interactable = true;
 ball.addEventListener("click", ()=>{
-    smallCircle.classList.toggle('turn-animation');
     ball.classList.toggle('turn-animation');
+    // ball.classList.toggle('turn-animation');
     // console.log( smallCircle.classList.toggle('turn-animation'));
  
     
-    if (smallCircle.classList.contains('turn-animation')
+    if (ball.classList.contains('turn-animation')
     //  && interactable ==true
      ){
+        rollSFX.currentTime=0;
+        rollSFX.play();
+        
         getRandomInt();   
         // if (orderNumber <= 9){
         //     number= Math.floor(Math.random() * 10);
@@ -61,13 +65,16 @@ ball.addEventListener("click", ()=>{
         // console.log(number);
 
 
-        setTimeout(()=>{
+        // setTimeout(()=>{
+          
+                    resultText.classList.add('fade-in');
                     resultText.innerHTML = foods[orderNumber];
-                }, 1000);
+                // }, 1000);
         
     }
-    else if (!smallCircle.classList.contains('turn-animation')){
-        resultText.innerHTML = 'Just pick For Fuck Sake';
+    else if (!ball.classList.contains('turn-animation')){
+        resultText.classList.remove('fade-in');
+        resultText.innerHTML = '';
         // smallCircle.classList.add('turn-animation');
         
     }
