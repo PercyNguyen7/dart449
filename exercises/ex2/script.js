5c3faf57ef355a6d3b8a7f5bc05ead27c74e4063
bro <percy.nguyen7@gmail.com>
2023-02-07 00:01:30 -0500

    update ex2

---
+++ exercises/ex2/script.js
@@ -0,0 +1,102 @@
+// code inspired by https://codepen.io/jacobhernandez08/pen/XwGqZw
+
+fetchResults(`https://www.reddit.com/r/all/top/.json`);
+
+// const parentDiv = document.querySelector('#parent-div');
+
+subreddit = "all";
+submit = document.querySelector('.button');
+
+submit.addEventListener('click', function(e){
+  
+  theSubreddit = document.querySelector('.subreddit').value;
+  
+  if(theSubreddit == null || theSubreddit.length < 1) {
+    subreddit = "all";
+  }
+  else {
+    subreddit = theSubreddit;
+  }
+  
+  console.log(subreddit);
+  fetchResults(`https://www.reddit.com/r/${subreddit}/new/.json`);
+  
+});
+
+async function fetchResults(url) {
+  fetch(url)
+  .then(response=>response.json()
+    // function(response) {
+    // document.querySelector('.content').innerHTML = 'Fetching results';
+    // return response.json();
+    // response=>response.json();
+  // }
+  )
+  .then(
+    json => {
+    // or function (myJson) {
+    renderResults(json);
+    // console.log(myJson);
+  
+  });
+}
+
+function renderResults(json) {
+  posts = json.data.children;
+  console.log(posts);
+  
+  if (document.getElementById(`parent-div`)){
+    document.getElementById('parent-div').remove();
+  }
+
+      let parentDiv = document.createElement('div');
+      parentDiv.id ='parent-div';
+  for (let i = 0; i< posts.length; i++){
+
+    if (posts[i].data.post_hint === `image`){
+    let ElWrapper = document.createElement('div');
+    // ElWrapper.classList.add(`el-wrapper`);
+    let figure = document.createElement('figure');
+    let image = document.createElement('img');
+    if (posts[i].data.post_hint === `null`){
+    }
+    image.src = posts[i].data.url_overridden_by_dest;
+    console.log(image.src);
+    figure.appendChild(image);
+    ElWrapper.appendChild(figure);
+    parentDiv.appendChild(ElWrapper);
+    console.log();
+    }
+    else if (posts[i].data.post_hint === `hosted:video`){
+      let ElWrapper = document.createElement('div');
+    // ElWrapper.classList.add(`el-wrapper`);
+    let figure = document.createElement('figure');
+    let video = document.createElement('video');
+    video.setAttribute("controls","controls")
+    video.src = posts[i].data.secure_media.reddit_video.fallback_url;
+    
+
+    figure.appendChild(video);
+    ElWrapper.appendChild(figure);
+    parentDiv.appendChild(ElWrapper);
+    }
+  }
+  document.body.appendChild(parentDiv);
+}
+
+
+ 
+
+
+
+// function renderResults(json) {
+//   posts = json.data.children;
+//   console.log(posts);
+//   let html = `<ol> 
+//   ${posts.map(
+//       post => `<li>${post.data.title} <a href=${post.data.url} target='_blank'>Link</a></li>`
+//     ).join('')}
+//   </ol>`;
+  
+//   document.querySelector('.content').innerHTML = html;
+// }
